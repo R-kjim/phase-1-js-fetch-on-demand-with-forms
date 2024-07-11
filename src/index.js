ecdbd05de3f9185c0ece31329013aaa52da81d54
R-kjim <rkayjim@gmail.com>
2024-07-11 16:36:06 +0300

    using fetch with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  const inputForm=document.querySelector("form")
+  let formInput=document.querySelector("input")
+  inputForm.addEventListener("submit",(event)=>{
+    event.preventDefault();console.log(formInput.value)
+
+    fetch(`http://localhost:3000/movies/${formInput.value}`)
+    .then(res=>res.json())
+    .then(data=>{
+        const title=document.querySelector("section#movieDetails h4")
+        const summary=document.querySelector("section#movieDetails p")
+        title.innerText=data.title
+        summary.innerText=data.summary
+    },
+    inputForm.reset()
+)
+
+
+
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
